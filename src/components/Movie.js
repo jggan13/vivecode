cedaedf224d38e25ec448562c9297eaa4fca0fb1
Jose Ganchozo <joseganchozo@outlook.com>
2019-08-26 19:47:31 -0500

    test

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 function Movie({movie}) {
     const { titulo, urlDescarga, urlImage } = movie;
-    const imagen = `https://testservicesapi.azurewebsites.net/Images/${urlImage}`;
+    const imagen = `http://jganchozo01-001-site3.ctempurl.com/Images/${urlImage}`;
+    //https://testservicesapi.azurewebsites.net
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
