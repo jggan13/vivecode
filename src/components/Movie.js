8a5d22926b9fbc27fa76aa63cb7359fca738e347
Jose Ganchozo <joseganchozo@outlook.com>
2019-08-26 19:49:02 -0500

    rollback test

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 function Movie({movie}) {
     const { titulo, urlDescarga, urlImage } = movie;
-    const imagen = `http://jganchozo01-001-site3.ctempurl.com/Images/${urlImage}`;
-    //https://testservicesapi.azurewebsites.net
+    const imagen = `https://testservicesapi.azurewebsites.net/Images/${urlImage}`;
     return (
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
